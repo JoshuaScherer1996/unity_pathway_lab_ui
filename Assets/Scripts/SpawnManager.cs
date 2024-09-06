8c097d5b48bf936c48c191fb9e8f33bb3648dae6
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-09-06 17:43:59 +0200

    implemeted the functionality of enemies spawning every second

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,25 +1,38 @@
+using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnManager : MonoBehaviour
 {
     // Declaring and initializing the variables and constants.
     public const float GlobalBound = 25f;
     public GameObject enemyPrefab;
-    
-    // Start is called before the first frame update.
+    private bool _gameOver = false;
+
     private void Start()
     {
-        
-        SpawnEnemy();
+        StartCoroutine(SpawnInterval());
     }
 
     private void SpawnEnemy()
     {
+        // Generates two random Positions within the global bounds.
         var spawnRangeX = Random.Range(-GlobalBound, GlobalBound);
         var spawnRangeZ = Random.Range(-GlobalBound, GlobalBound);
 
+        // Creates a new position based on the global bounds.
         var randomPos = new Vector3(spawnRangeX, 0.54f, spawnRangeZ);
         
+        // Creates a new enemy object.
         Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
+    }
+
+    private IEnumerator SpawnInterval()
+    {
+        while(!_gameOver)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
